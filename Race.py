25b3e096650aa500f3cb07d0d1c551311496936b
Donghyun Lee <u1538647@utah.edu>
2025-03-12 15:57:59 +0900

    A2 - Horse Race
    Assignment for A2 horse race

--- Race.py
+++ Race.py
@@ -7,7 +7,7 @@ import random
 
 class Horse:
     def __init__(self, speed, y_pos, image_file, window):
-        self.x_pos = 50
+        self.x_pos = 0
         self.y_pos = y_pos
         self.image = Image(Point(self.x_pos, self.y_pos), image_file)
         self.window = window
